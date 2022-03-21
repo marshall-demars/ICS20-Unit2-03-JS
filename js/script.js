a18afc6dde83d66a66a1d07d77e6ca64e4f1a732
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-21 17:28:34 +0000

    3 files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Created by: Marshall
+ * Created on: Mar 2022
+ * This CSS file contains the JS functions for index.html 
+*/
+
+function enterClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = parseInt(document.getElementById("street-name").value)
