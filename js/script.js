ef67e35a0dc5d7addb34ff475147fd168fbce195
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-22 16:57:00 +0000

    3 files

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ function enterClicked() {
   // input
   const streetNumber = document.getElementById("street-number").value
   const streetName = parseInt(document.getElementById("street-name").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "<p>Your street address is: " + street-number + street-name + ".</p>"
+}
