9ea60d193295d1ba9aeecd1b39f311813bbfed50
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-22 17:10:09 +0000

    2 files

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
  * This CSS file contains the JS functions for index.html 
 */
 
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
 function enterClicked() {
   // input
   const streetNumber = parseInt(document.getElementById("street-number").value)
